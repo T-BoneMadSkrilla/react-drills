d9d17457828c85e92a9c72d2749f1644b0eaee85
T-BoneMadSkrilla <tylereftink@gmail.com>
2018-09-28 16:39:34 -0500

    first three done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(){
+
+    super();
+
+    this.state = { names: ['Tyler', ' Jerry', ' Ben']}
+
+  }
+
+
   render() {
+
+    let coolDudez = this.state.names.map( (element, index) => {return (<h2 key={index}> {element} </h2>)})
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          
+          { coolDudez }
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
